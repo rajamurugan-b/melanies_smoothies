dda8c43f3e5917e34ef13565dc896be1464e1d43
rajamurugan-b <144204856+rajamurugan-b@users.noreply.github.com>
2024-09-01 18:34:04 +0530

    Update streamlit_app.py
    updated from max_selection=5 to max_selections=5

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients: '
     , my_dataframe
-    #, max_selection=5
+    , max_selections=5
     )
 
 if ingredients_list:
